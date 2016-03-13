be50060e8209f42ea9cf3d7195181da7211ec23e
Trung Ho Quang <leetrunghoo@gmail.com>
2016-03-14 01:41:53 +0700

    enhance style, fix bugs, change side bar

--- assets/js/main.js
+++ assets/js/main.js
@@ -43,6 +43,68 @@ $.fn.typingAnimation = function(callback) {
     };
 };
 
+
+// detect if IE : from http://stackoverflow.com/a/16657946		
+var ie = (function() {
+    var undef, rv = -1; // Return value assumes failure.
+    var ua = window.navigator.userAgent;
+    var msie = ua.indexOf('MSIE ');
+    var trident = ua.indexOf('Trident/');
+
+    if (msie > 0) {
+        // IE 10 or older => return version number
+        rv = parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+    } else if (trident > 0) {
+        // IE 11 (or newer) => return version number
+        var rvNum = ua.indexOf('rv:');
+        rv = parseInt(ua.substring(rvNum + 3, ua.indexOf('.', rvNum)), 10);
+    }
+
+    return ((rv > -1) ? rv : undef);
+}());
+
+
+// disable/enable scroll (mousewheel and keys) from http://stackoverflow.com/a/4770179					
+// left: 37, up: 38, right: 39, down: 40,
+// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+var keys = [32, 37, 38, 39, 40],
+    wheelIter = 0;
+
+function preventDefault(e) {
+    e = e || window.event;
+    if (e.preventDefault)
+        e.preventDefault();
+    e.returnValue = false;
+}
+
+function keydown(e) {
+    for (var i = keys.length; i--;) {
+        if (e.keyCode === keys[i]) {
+            preventDefault(e);
+            return;
+        }
+    }
+}
+
+function touchmove(e) {
+    preventDefault(e);
+}
+
+function wheel(e) {
+    preventDefault(e);
+}
+
+function disable_scroll() {
+    window.onmousewheel = document.onmousewheel = wheel;
+    document.onkeydown = keydown;
+    document.body.ontouchmove = touchmove;
+}
+
+function enable_scroll() {
+    window.onmousewheel = document.onmousewheel = document.onkeydown = document.body.ontouchmove = null;
+}
+
+
 $(function() {
     // show article after document ready
     document.querySelector('article').style.display = 'block';
@@ -90,6 +152,13 @@ $(function() {
     // Modal toggle behavior
     $('.modal-toggle-wrapper').click(function() {
         $(this).toggleClass('active');
+        if ($(this).hasClass('active')) {
+            disable_scroll()
+            $('#menu-button').fadeOut();
+        } else {
+            enable_scroll();
+            $('#menu-button').fadeIn();
+        }
         $('.modal-toggle-bubble').toggleClass('active');
         $('.modal-toggle-bubbleShadow').toggleClass('active');
         $('.modal-toggle-close-wrapper').toggleClass('active');
---
+++ assets/js/sidebar.js
@@ -0,0 +1,75 @@
+(function() {
+    // sidebar
+    var bodyEl = document.body,
+        container = document.getElementById('container'),
+        openbtn = document.getElementById('open-button'),
+        closebtn = document.getElementById('close-button'),
+        isOpen = false,
+
+        morphEl = document.getElementById('morph-shape'),
+        s = Snap(morphEl.querySelector('svg'));
+    path = s.select('path');
+    initialPath = this.path.attr('d'),
+        steps = morphEl.getAttribute('data-morph-open').split(';');
+    stepsTotal = steps.length;
+    isAnimating = false;
+
+    function init() {
+        initEvents();
+    }
+
+    function initEvents() {
+        // openbtn.addEventListener('click', toggleMenu);
+        // if (closebtn) {
+        //     closebtn.addEventListener('click', toggleMenu);
+        // }
+
+        // close the menu element if the target it´s not the menu element or one of its descendants..
+        container.addEventListener('click', function(ev) {
+            var target = ev.target;
+            if (isOpen && target !== openbtn) {
+                toggleMenu();
+            }
+        });
+
+        $('#menu-button').click(function() {
+            toggleMenu();
+        });
+    }
+
+    function toggleMenu() {
+        if (isAnimating) return false;
+        isAnimating = true;
+        if (isOpen) {
+            classie.remove(bodyEl, 'show-menu');
+            // animate path
+            setTimeout(function() {
+                // reset path
+                path.attr('d', initialPath);
+                isAnimating = false;
+                enable_scroll();
+            }, 300);
+        } else {
+            disable_scroll();
+            classie.add(bodyEl, 'show-menu');
+            // animate path
+            var pos = 0,
+                nextStep = function(pos) {
+                    if (pos > stepsTotal - 1) {
+                        isAnimating = false;
+                        return;
+                    }
+                    path.animate({ 'path': steps[pos] }, pos === 0 ? 400 : 500, pos === 0 ? mina.easein : mina.elastic, function() { nextStep(pos); });
+                    pos++;
+                };
+
+            nextStep(pos);
+        }
+        isOpen = !isOpen;
+        $('#menu-button').toggleClass('open');
+    }
+
+    init();
+
+
+})();
