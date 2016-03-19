b7eefb5fb9f96b47455f85f097c1a581efb8126a
leetrunghoo <leetrunghoo@gmail.com>
2016-03-20 01:47:20 +0700

    enhance home page (change image, transition)

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,19 +44,19 @@ $.fn.typingAnimation = function(callback) {
 };
 
 // http://stackoverflow.com/questions/9847580/how-to-detect-safari-chrome-ie-firefox-and-opera-browser
-    // Opera 8.0+
+// Opera 8.0+
 var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
-    // Firefox 1.0+
+// Firefox 1.0+
 var isFirefox = typeof InstallTrigger !== 'undefined';
-    // At least Safari 3+: "[object HTMLElementConstructor]"
+// At least Safari 3+: "[object HTMLElementConstructor]"
 var isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;
-    // Internet Explorer 6-11
-var isIE = /*@cc_on!@*/false || !!document.documentMode;
-    // Edge 20+
+// Internet Explorer 6-11
+var isIE = /*@cc_on!@*/ false || !!document.documentMode;
+// Edge 20+
 var isEdge = !isIE && !!window.StyleMedia;
-    // Chrome 1+
+// Chrome 1+
 var isChrome = !!window.chrome && !!window.chrome.webstore;
-    // Blink engine detection
+// Blink engine detection
 var isBlink = (isChrome || isOpera) && !!window.CSS;
 window.os = {
     isOpera: isOpera,
@@ -135,11 +135,13 @@ $(function() {
 
     // add typing animation for home page
     $('#welcomeContainer').typingAnimation(function() {
-        var $section = $('#welcomeContainer').parent();
-        $('#welcomeContainer').addClass('is-darkBackgrounded');
-        $section.addClass('is-darkBackgrounded');
-        $section.find('.section-backgroundImage').fadeIn(500);
-        $section.find('.section-gradient').fadeIn(500);
+        setTimeout(function() {
+            var $section = $('#welcomeContainer').parent();
+            $('#welcomeContainer').addClass('is-darkBackgrounded');
+            $section.addClass('is-darkBackgrounded');
+            $section.find('.section-backgroundImage').removeClass('opacity-0');
+            $section.find('.section-gradient').removeClass('opacity-0');
+        }, 200);
     });
 
     // scroll animation
