b56ae6020bfb4040b6868413f1bf72a3fa7071eb
leetrunghoo <leetrunghoo@gmail.com>
2016-09-06 20:05:57 +0700

    enhance Flickr demo

--- demo/flickr/handleFlickr.js
+++ demo/flickr/handleFlickr.js
@@ -1,7 +1,7 @@
 $(function() {
     var apiKey = '986e904143a37c668876552671aacde9',
         userId = localStorage.getItem('demo_userId') || 'hin-stone', //99002729@N07
-        // photo quality = 3: low, 5: med, 8: high, 9: xhigh
+        // photo quality = 1: very low,  3: low, 5: med, 8: high, 9: xhigh
         qThumb = localStorage.getItem('demo_qThumb') || 5,
         qPhoto = localStorage.getItem('demo_qPhoto') || 8,
         perPage = 100,
@@ -36,19 +36,9 @@ $(function() {
         percentPosition: true
     });
 
-    // init lightGallery
-    var gallery = initLightGallery();
 
-    function initLightGallery() {
-        return $container.lightGallery({
-            thumbnail: true,
-            animateThumb: true,
-            showThumbByDefault: true,
-            showAfterLoad: false,
-            getCaptionFromTitleOrAlt: false,
-            hash: false
-        }).data('lightGallery');
-    }
+    // init lightGallery
+    var gallery;
 
     $('.pagination').pagination({
         items: 0,
@@ -63,8 +53,6 @@ $(function() {
         onPageClick: function(pageNumber, event) {
             console.log(pageNumber);
             $container.empty();
-            gallery.destroy(true);
-            gallery = initLightGallery();
             loadPage(pageNumber);
             $('.pagination.bottom').hide();
             $(document).scrollTop(0);
@@ -95,18 +83,17 @@ $(function() {
             $img.show();
             $container.masonry('appended', $img, true);
             $container.masonry('layout');
-            gallery.$items = gallery.$items.add($a);
-            gallery.init();
+            initLightGallery();
             $a.click();
             $container.one('onBeforeClose.lg', function() {
                 var newUrl = deleteUrlParam('photo_url');
                 changeUrl(newUrl);
-                reloadPhotos();
+                loadPage(1);
             });
         });
     } else {
         console.info('------start loading photos------');
-        startLoadImages();
+        loadPage(1);
     }
 
     // Remodal event
@@ -134,10 +121,7 @@ $(function() {
             flagReloadPhotos = true;
         }
         if (flagReloadPhotos) {
-            $container.empty();
-            gallery.destroy(true);
-            gallery = initLightGallery();
-            startLoadImages();
+            loadPage(1);
         }
     });
     $(document).on('cancellation', '.remodal', function() {
@@ -146,42 +130,38 @@ $(function() {
         $('#selectPhotoQuality').val(qPhoto);
     });
 
-    function reloadPhotos() {
-        $container.empty();
-        gallery.destroy(true);
-        gallery = initLightGallery();
-        startLoadImages();
-    }
-
     // when view photo detail => load larger photo
     $container.on('onAfterSlide.lg', function() {
         $('#tempImg').remove();
-        setTimeout(function() {
-            var thumbLink = $('.lg-thumb-item.active img').attr('src');
-            var detailLink = $('.lg-current .lg-img-wrap img').attr('src');
-            if (!detailLink) {
-                return;
-            }
-            // change url without refreshing
-            var newUrl = deleteUrlParam('photo_url') + '&photo_url=' + detailLink;
-            changeUrl(newUrl);
-            // check cached img
-            if (!listLoadedImg[detailLink]) {
-                $('.lg-current .lg-img-wrap img').attr('src', thumbLink); // use Thumbnail photo for detail while loading
-                $container.append('<img src="' + detailLink + '" id="tempImg" style="display: none">');
-                $('#tempImg').imagesLoaded(function() {
-                    // check current viewing thumb photo has same ID with detail photo
-                    var currentImgUrl = $('.lg-current .lg-img-wrap img').attr('src') || '';
-                    var currentImgId = currentImgUrl.substring(currentImgUrl.lastIndexOf('/') + 1, currentImgUrl.lastIndexOf('.'));
-                    currentImgId = currentImgId.split('_')[0];
-                    if (detailLink.indexOf(currentImgId) > 0) {
-                        $('.lg-current .lg-img-wrap img').attr('src', detailLink);
-                    }
-                    listLoadedImg[detailLink] = true;
-                    $('#tempImg').remove();
-                });
-            }
-        }, 100);
+        var thumbLink = $('.lg-thumb-item.active img').attr('src');
+        var detailLink = $('.lg-current .lg-img-wrap img').attr('src');
+        if (!detailLink) {
+            return;
+        }
+        // change url without refreshing
+        var newUrl = deleteUrlParam('photo_url') + '&photo_url=' + detailLink;
+        changeUrl(newUrl);
+        // load thumb photo while loading detail photo
+        // if (detailLink === thumbLink) {
+        //     // no need to load detail photo
+        //     return;
+        // }
+        // check cached img
+        // if (!listLoadedImg[detailLink]) {
+        //     $('.lg-current .lg-img-wrap img').attr('src', thumbLink); // use Thumbnail photo for detail while loading
+        //     $container.append('<img src="' + detailLink + '" id="tempImg" style="display: none">');
+        //     $('#tempImg').imagesLoaded(function() {
+        //         // check current viewing thumb photo has same ID with detail photo
+        //         var currentImgUrl = $('.lg-current .lg-img-wrap img').attr('src') || '';
+        //         var currentImgId = currentImgUrl.substring(currentImgUrl.lastIndexOf('/') + 1, currentImgUrl.lastIndexOf('.'));
+        //         currentImgId = currentImgId.split('_')[0];
+        //         if (detailLink.indexOf(currentImgId) > 0) {
+        //             $('.lg-current .lg-img-wrap img').attr('src', detailLink);
+        //         }
+        //         listLoadedImg[detailLink] = true;
+        //         $('#tempImg').remove();
+        //     });
+        // }
     });
     $container.on('onCloseAfter.lg', function() {
         var newUrl = deleteUrlParam('photo_url');
@@ -203,19 +183,27 @@ $(function() {
                 });
             }
         }
-        if ($(document).scrollTop() > $(window).height()) {
+        // if ($(document).scrollTop() === $(window).height()) {
             $('.pagination.bottom').show();
-        }
+        // }
     });
 
-    function startLoadImages() {
-        startPage = 0;
-        loadPage(++startPage, function() {
-            // make sure body has scroll therefore be able to do infinitescroll
-            if (document.body.scrollHeight <= window.innerHeight) {
-                loadPage(++startPage);
-            }
-        });
+    // prevent view photo detail while loading new photos
+    $(document).on('click', 'a.waiting', function(e) {
+        e.preventDefault();
+    });
+
+    function initLightGallery() {
+        if (gallery) {
+            gallery.destroy(true);
+        }
+        gallery = $container.lightGallery({
+            thumbnail: true,
+            animateThumb: true,
+            showThumbByDefault: true,
+            showAfterLoad: false,
+            hash: false
+        }).data('lightGallery');
     }
 
     function loadPage(page, callback) {
@@ -228,9 +216,19 @@ $(function() {
                 startPhotoIndex = 0;
                 $('.pagination').pagination('updateItems', response.photos.total);
                 loadImages(function() {
-                    NProgress.done();
-                    if (callback) {
-                        callback();
+                    // make sure body has scroll therefore be able to do infinitescroll
+                    if (document.body.scrollHeight <= window.innerHeight + 100) {
+                        loadImages(function() {
+                            NProgress.done();
+                            if (callback) {
+                                callback();
+                            }
+                        });
+                    } else {
+                        NProgress.done();
+                        if (callback) {
+                            callback();
+                        }
                     }
                 });
             } else {
@@ -251,27 +249,26 @@ $(function() {
             var photo = photos[i];
             var link = 'https://farm' + photo.farm + '.staticflickr.com/' + photo.server + '/' + photo.id + '_' + photo.secret + arrQuality[+qThumb] + '.jpg';
             var link_detail = 'https://farm' + photo.farm + '.staticflickr.com/' + photo.server + '/' + photo.id + '_' + photo.secret + arrQuality[+qPhoto] + '.jpg';
-            imgages += '<a href="' + link_detail + '" ><img class="grid-item " src="' + link + '"" alt="' + photo.title + '" style="display: none" ></a>';
+            imgages += '<a class="waiting" href="' + link_detail + '" ><img class="grid-item " src="' + link + '"" alt="' + photo.title + '" style="display: none" ></a>';
         }
-        appendImages($(imgages), callback);
-    }
-
-    function appendImages($newEle, callback) {
+        var $newEle = $(imgages);
         $container.append($newEle);
         $newEle.each(function(index) {
             var $a = $(this);
             var $img = $a.find('img');
             $img.imagesLoaded(function(ele) {
-                // console.info('loaded img: ', $img.attr('src'));
                 $img.show();
                 $container.masonry('appended', $img, true);
                 $container.masonry('layout');
-                gallery.$items = gallery.$items.add($a);
-                gallery.init();
+                // gallery.$items = gallery.$items.add($a);
+                // gallery.init();
             });
         });
+        // done loaded images => reinit LightGallery
         $newEle.imagesLoaded(function() {
-            console.info('====loaded all====');
+            console.info('end load PhotoIndex: ', +startPhotoIndex - perPart + ' - ' + startPhotoIndex);
+            $newEle.find('a').removeClass('waiting');
+            initLightGallery();
             if (callback) {
                 callback();
             }
