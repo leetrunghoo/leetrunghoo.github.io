e1b1ae7b158c331dd19d2312d6500d3fc2ed6dea
leetrunghoo <leetrunghoo@gmail.com>
2016-10-03 22:45:51 +0700

    update React demo & add new React demo (reverted from commit e56057ec5f1520b1acfb0230d51af6597cb406f5) (reverted from commit 9e52fd3dfac868857930bd1c505f41edc33d7ca4) (reverted from commit 048be8d3f21e7ab54c6a3eb4f328d588231db0c0)

--- demo/react/js/components/SearchBar.js
+++ demo/react/js/components/SearchBar.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+  constructor() {
+    super();
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(e) {
+    // delegate events to component
+    this.props.onUserInput(
+      e.target.value
+    );
+  }
   render() {
     return (
       <div className="search-bar">
         <form>
-          <input type="text" placeholder="Search..."
-                  onChange={(e) => this.props.onUserInput(e.target.value)}/>
+          <input type="text" placeholder="Search..." value={this.props.filterText} 
+                  onChange={this.handleChange}/>
         </form>
       </div>
     );
